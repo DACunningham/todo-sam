81885ab7632d1e3518fe84fac835ae06207c4db4
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-05-09 19:53:28 +0100

    Add Event Input and Minor Logic Change

--- hello_world/app.py
+++ hello_world/app.py
@@ -33,10 +33,11 @@ def lambda_handler(event, context):
 
     #     raise e
 
+    print(event)
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": event["body"],
             # "location": ip.text.replace("\n", "")
         }),
     }
